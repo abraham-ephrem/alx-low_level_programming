558ed1d4bdd01609f813a9d82ae08c349c4591a2
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-10 18:58:32 +0300

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This is the main function
+ * Return: 1
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
