94994487a45caeb84011587d02b1401d18b11858
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-11 17:03:45 +0300

    Modified 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	char char_array[] = "abcdefghijklmnopqrstuvwxyz\n";
-
-	for (int i = 0; i < 27; i++)
+	int i = 0;
+	for (i; i < 27; i++)
 		putchar(char_array[i]);
 
 	return (0);
