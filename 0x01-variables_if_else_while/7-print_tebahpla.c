ee309ea22c51d5e80adc6673c6e3f7ec27241c1e
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-11 18:59:57 +0300

    Added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int char_array = "\nabcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 27; i >= 0; i--)
+		putchar(char_array[i]);
+
+	return (0);
+}
