43cbe0ea8c69cd819c3d9cff53ade626bd6cd7db
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-11 19:11:19 +0300

    Added 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,9 +9,9 @@ int main(void)
 	char char_array[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 255555; i >= 0; i--)
+	for (i = 25; i >= 0; i--)
 		putchar(char_array[i]);
-
 	putchar('\n');
+
 	return (0);
 }
