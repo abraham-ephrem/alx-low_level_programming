7bfa6ce4a01f6dfad5d4bb4ff404f189bd0ad523
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 17:35:10 +0300

    mod

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,3 +9,17 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
+/**
+ * print_alphabet - prints small letters
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar('i');
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,18 +10,6 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-/**
- * print_alphabet - prints small letters
- *
- * Return: 0
- */
-void print_alphabet(void)
-{
-	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar('i');
-
-	_putchar('\n');
-}
+void print_alphabet(void);
 #endif
