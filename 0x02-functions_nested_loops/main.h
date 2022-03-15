79845dd2bffcd6ab2ec597784d68946f40872146
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 16:52:25 +0300

    Added 1-aphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,16 +15,15 @@ int _putchar(char c);
  *
  * Return: void
  */
-void print_alphabet(void);
-/*
- *  {
- *	int i;
- *       for (i = 'a'; i <= 'z'; i++)
- *		_putchar(i);
- *
- *	_putchar('\n');
- *
- *	return;
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
 }
- */
+
 #endif
