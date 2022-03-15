d65c546cf4208cb4776d969095acc16d41a42233
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 17:00:41 +0300

    Added 1-aphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,3 +10,18 @@ int main(void)
 
 	return (0);
 }
+/**
+ * print_alphabet - prints the alphabets
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+}
