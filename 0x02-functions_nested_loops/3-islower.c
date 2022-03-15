72ba813e962dd0c8c5aec4590143d5e2f3e92bef
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 18:41:49 +0300

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - checks if a character is small letter
+ * @c: The character to be checked
+ *
+ * Return: 1 if the character is lower case or 0 otherwise
+ */
+int _islower(int c)
+{
+	return (islower(c));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,4 +22,11 @@ void print_alphabet(void);
  * Return: void
  */
 void print_alphabet_x10(void);
+/**
+ * _islower - checks for small letters
+ * @c: The letter to be checked
+ *
+ * Return: 1 if the letter is a small letter or 0 otherwise
+ */
+int _islower(int c);
 #endif
