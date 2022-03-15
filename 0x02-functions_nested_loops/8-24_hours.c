94a47ffe56fecdad8985001603bcfa880b5804fe
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 21:58:31 +0300

    Added 8-jack_bauer.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,12 +19,12 @@ void jack_bauer(void)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(':');
-					putchar(k + '0');
-					putchar(l + '0');
-					putchar('\n');
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
 				}
 			}
 		}
