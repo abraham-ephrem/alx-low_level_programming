d0a6d6747ed7cb0ac67f1b6b922341785af6440e
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 23:38:44 +0300

    Added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,27 +15,23 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (result <= 9)
-			{
+			if (result == 0)
 				_putchar(result + '0');
-				if (!(j == 9))
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
+			else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
-				if (!(j == 9))
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
