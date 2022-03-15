545828afe7851217322a4bf48434a1b43294ccc3
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 13:54:23 +0300

    Added main.h, 0-putchar.c and1-aphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	char string[] = "_putchar";
+	char string[] = "_putchar\n";
 	int i;
 
 	for (i = 0; i < 9; i++)
 		_putchar(string[i]);
-	_putchar('\n');
 
 	return (0);
 }
