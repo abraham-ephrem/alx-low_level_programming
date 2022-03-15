07044554c4a6e34a916b337d4fc82a61ecb12112
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-15 13:49:12 +0300

    Added main.h, 0-putchar.c and1-aphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,10 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_alphabet - prints the alphabets
