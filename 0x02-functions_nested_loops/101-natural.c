3f7c438a3f8b9180668e137221a43f379fdf69a2
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-16 09:54:07 +0300

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int total = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			total += i;
+	}
+	return (0);
+}
