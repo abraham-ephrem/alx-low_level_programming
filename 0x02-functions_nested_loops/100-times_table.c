bca625e3a94410e6eae351a148a45bf9d3e90641
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-16 09:44:13 +0300

    Added advanced task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -49,7 +49,7 @@ void print_times_table(int n)
 				else if (result >= 100)
 				{	_putchar(',');
 					_putchar(' ');
-					_putchar((result / 10) + '0');
+					_putchar((result / 100) + '0');
 					_putchar(((result / 10) % 10) + '0');
 					_putchar((result % 10) + '0');
 				}
