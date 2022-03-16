e7c5e69f0e8259270ac31a1ccd5f43dad7bf064d
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-16 12:48:02 +0300

    Added 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long n1 = 1;
+	unsigned long n2 = 2;
+	unsigned long current_fib;
+	int i;
+
+	printf("%lu, %lu, ", n1, n2);
+	for (i = 1; i <= 96; i++)
+	{
+		current_fib = n1 + n2;
+		if (i == 96)
+			printf("%lu", current_fib);
+		else
+			printf("%lu", current_fib);
+		n1 = n2;
+		n2 = current_fib;
+	}
+	printf("\n");
+	return (0);
+}
