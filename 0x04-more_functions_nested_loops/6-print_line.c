c22e1e6991154cb472aad5d6924563cce43a7863
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-17 14:53:05 +0300

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+/**
+ * print_line - prints a line
+ * @n: the length of the line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
