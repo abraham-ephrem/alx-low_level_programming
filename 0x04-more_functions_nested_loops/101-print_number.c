2819fd3ccfa995f7f95e7f0ee7122f96412c68d9
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-17 18:32:57 +0300

    Modification

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * print_numbers_100000 - prints an integer
+ * @n: the integer
+ * Return: 0
+ */
+void print_numbers_100000(int n)
+{
+	_putchar((num / 100000) + '0');
+	_putchar((num / 10000 % 10) + '0');
+	_putchar((num / 1000 % 10) + '0');
+	_putchar((num / 100 % 10) + '0');
+	_putchar((num / 10 % 10) + '0');
+	_putchar((num % 10) + '0');
+}
+/**
+ * print_numbers_1000000 - prints numbers
+ * @n: the number
+ * Return: void
+ */
+void print_numbers_1000000(int n)
+{
+	_putchar((num / 1000000) + '0');
+	_putchar((num / 100000 % 10) + '0');
+	_putchar((num / 10000 % 10) + '0');
+	_putchar((num / 1000 % 10) + '0');
+	_putchar((num / 100 % 10) + '0');
+	_putchar((num / 10 % 10) + '0');
+	_putchar((num % 10) + '0');
+}
+/**
  * print_number - prints an integer
  * @n: the integer
  * Return: 0
@@ -27,11 +56,23 @@ void print_number(int n)
 		_putchar((num / 10 % 10) + '0');
 		_putchar((num % 10) + '0');
 	}
-	else
+	else if (num >= 1000 && num < 10000)
 	{
 		_putchar((num / 1000) + '0');
 		_putchar((num / 100 % 10) + '0');
 		_putchar((num / 10 % 10) + '0');
 		_putchar((num % 10) + '0');
 	}
+	else if (num >= 10000 && num < 100000)
+	{
+		_putchar((num / 10000) + '0');
+		_putchar((num / 1000 % 10) + '0');
+		_putchar((num / 100 % 10) + '0');
+		_putchar((num / 10 % 10) + '0');
+		_putchar((num % 10) + '0');
+	}
+	else if (num >= 100000 && num < 1000000)
+		print_numbers_100000(num);
+	else
+		print_numbers_1000000(num);
 }
