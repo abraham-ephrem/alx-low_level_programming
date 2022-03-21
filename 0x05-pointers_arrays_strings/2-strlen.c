c142a678818596381d47ea13e18755aa2b07cdb1
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-21 10:25:10 +0300

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - caluclates the length of a string
+ * @s: the address of the first character of the string
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
