7efac8094177cc90b83dce7ec4de9838558b12a1
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-21 17:33:43 +0300

    Added 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string
+ * Return: an integer from s
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
