c69db96e83d79f109f1a637a5f7d46212b083674
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-21 10:38:01 +0300

    Added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string
+ * @str: The address of the first character in the string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
