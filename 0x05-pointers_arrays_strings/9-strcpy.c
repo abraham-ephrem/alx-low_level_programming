0b26faca6b7c3b5a845735c3f9659937ac343ad7
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-21 17:09:13 +0300

    Added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copies a string
+ * @dest: The destination of the copy
+ * @src: the original string
+ * Return: the copy of the src
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
