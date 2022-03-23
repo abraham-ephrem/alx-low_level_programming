0ba3e2a929778e6e732c4a9577f4875e99a7e875
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-23 09:06:35 +0300

    Added 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concats two strings
+ * @dest: the second string
+ * @src: the first string
+ * Return: the concatnated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
