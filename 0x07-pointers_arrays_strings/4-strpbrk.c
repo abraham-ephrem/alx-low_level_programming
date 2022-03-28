c7e9924329f03acebb61f669b2c53d5edfc3642d
abraham-ephrem <ephremabraham8997@gmail.com>
2022-03-28 16:20:28 +0300

    modification

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strpbrk - returns the first occurence of a character in accept
  * @s: the string to be searched
@@ -7,25 +8,6 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
-	char *result;
-
-	while (s[i] != '\0')
-	{
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])
-			{
-				result = &s[i];
-				goto end;
-			}
-			j++;
-		}
-		i++;
-		j = 0;
-	}
-end:
-	return (result);
+	return (strpbrk(s, accept));
 }
 
