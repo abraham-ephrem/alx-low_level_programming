87ab30d3e6e5a51d46f501509d3d195dd4cea4a2
abraham-ephrem <ephremabraham8997@gmail.com>
2022-04-04 16:31:16 +0300

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,6 @@ char *create_array(unsigned int size, char c)
 	array_ptr = malloc(sizeof(char) * size);
 	if (array_ptr == NULL)
 		return (NULL);
-	array_ptr[1] = c;
+	array_ptr[0] = c;
 	return (array_ptr);
 }
