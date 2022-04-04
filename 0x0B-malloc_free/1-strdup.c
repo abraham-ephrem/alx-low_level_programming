3573aaf5fc0696857346dbca9aad8f1dbf09a4ad
abraham-ephrem <ephremabraham8997@gmail.com>
2022-04-04 17:15:18 +0300

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,16 @@
 char *_strdup(char *str)
 {
 	char *new_string;
+	int i;
+	int size = strlen(str);
 
 	if (str == NULL)
 		return (NULL);
-	new_string = malloc(sizeof(char) * ((strlen(str) + 1)));
+	new_string = malloc(sizeof(char) * size);
 	if (new_string == NULL)
 		return (NULL);
-	new_string = str;
+	for (i = 0; i < size; i++)
+		new_string[i] = str[i];
 	return (new_string);
 }
 
