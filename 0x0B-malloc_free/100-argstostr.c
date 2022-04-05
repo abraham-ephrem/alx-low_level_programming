f7fa65fcd2de5f9edc1e1c77b799428796b37d60
abraham-ephrem <ephremabraham8997@gmail.com>
2022-04-05 14:41:40 +0300

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,17 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * strlength - returns the combined length of strings
+ * @ac: the number of strings
+ * @av: an arrsy of strings
+ * Return: the length
+ */
 unsigned int strlength(int ac, char **av)
 {
 	int i;
 	unsigned int length = 0;
-	
+
 	for (i = 0; i < ac; i++)
 		length += strlen(av[i]);
 
 	return (length);
 }
-
+/**
+ * argstostr - prints the arguments passed on new line
+ * @ac: the number of arguments
+ * @av: the arguments
+ * Return: a new string containing the arguments
+ */
 char *argstostr(int ac, char **av)
 {
 	int i;
@@ -26,7 +37,7 @@ char *argstostr(int ac, char **av)
 	str = malloc(sizeof(char) * (unsigned int)size);
 	if (str == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
